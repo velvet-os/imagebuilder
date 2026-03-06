a00add3d812c1789d98c904e0c896efabbc30372
hexdump <hexdump0815@googlemail.com>
2026-03-06 13:10:39 +0100

    scripts,corsola: switch to pipewire by default
    pipewire seems to be stable enough meanwhile to make it the default

--- systems/chromebook_corsola/postinstall-chroot.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# some corsola devices created strange audio noises when run with
-# pulseaudio which were not there when run with pipewire, so lets
-# switch this device to pipewire by default ...
-apt-get -y install pipewire-audio
